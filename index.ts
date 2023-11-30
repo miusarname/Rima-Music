c1a9f73a99539bdb0c9e1762d63237d7b47de999
miusarname <oscarm.alvarezg@gmail.com>
2023-11-30 13:42:41 -0500

    feat: :sparkles: The dirname was created
    The "dirname" was created to "guide" in the execution.

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import { storage,upload } from "./controller/Storage.controller.js";
 import { tracks } from "./controller/routes/tracks.routes.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");

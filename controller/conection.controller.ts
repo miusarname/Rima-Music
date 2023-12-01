c90d556ac16653d84ba55394cc1c3680e845a3b4
miusarname <oscarm.alvarezg@gmail.com>
2023-12-01 16:08:23 -0500

    feat: :card_file_box: I modified the database connection driver, so that it connects to the environment variables and from there it gets what it needs.

--- controller/conection.controller.ts
+++ controller/conection.controller.ts
@@ -1,17 +1,21 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
 import { Connection } from "mysql2/typings/mysql/lib/Connection";
 
+dotenv.config();
+console.log(process.env.DB_USER,'db')
+
 export const db: Connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456",
-  database: "RimaMusic",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 db.connect((err) => {
-    if (err) {
-      console.error('Error al conectar a la base de datos: ' + err.stack);
-      return;
-    }
-    console.log('Conexión exitosa a la base de datos');
-  });
+  if (err) {
+    console.error("Error al conectar a la base de datos: " + err.stack);
+    return;
+  }
+  console.log("Conexión exitosa a la base de datos");
+});

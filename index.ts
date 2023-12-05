f55a051caa8c6c9af1069f67249662fe79744091
miusarname <oscarm.alvarezg@gmail.com>
2023-12-05 09:58:21 -0500

    feat: :sparkles: Routes and routing added to the main file

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import express, { Express, Request, Response } from "express";
 import { storage,upload } from "./controller/Storage.controller.js";
 import { tracks } from "./controller/routes/tracks.routes.js";
 import { artist } from "./controller/routes/artist.routes.js";
+import { album } from "./controller/routes/album.routes.js";
+import { user } from "./controller/routes/user.routes.js";
+import { crearToken,validarToken } from "./controller/helpers/jwt.controller.js";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import dotenv from "dotenv";
@@ -20,8 +23,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.use("/tracks", tracks);
-app.use("/artist", artist);
+app.get('/tk',crearToken);
+app.use("/tracks",validarToken, tracks);
+app.use("/artist", validarToken,artist);
+app.use("/album", validarToken,album);
+app.use("/user", validarToken,user);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);

ba9bac64745eedd3dcc29bf3e88307cb3fb8d62c
miusarname <oscarm.alvarezg@gmail.com>
2023-12-06 09:01:44 -0500

    feat: :sparkles: New request limiters (rate-limits) are created for different cases

--- controller/helpers/limiter.controller.ts
+++ controller/helpers/limiter.controller.ts
@@ -25,3 +25,105 @@ export let limitGrt = () => {
         }
     });
 }
+
+
+export let GetAll = () => {
+    return rateLimit({
+        windowMs: 60 * 1000,
+        max: 50,
+        standardHeaders: true,
+        legacyHeaders: false,
+        skip: (req: any, res: Response) => {
+            if (parseInt(req.headers["content-length"]) > 690) {
+                res.status(413).send({
+                    status: 413,
+                    message: "Tamaño de la solicitud alcanzado"
+                });
+                return true;
+            }
+            return false;
+        },
+        message: (req:any, res:Response) => {
+            res.status(429).send({
+                status: 429,
+                message: "Limite alcanzado"
+            });
+        }
+    });
+}
+
+
+export let search = () => {
+    return rateLimit({
+        windowMs: 30*60 * 1000,
+        max: 15,
+        standardHeaders: true,
+        legacyHeaders: false,
+        skip: (req: any, res: Response) => {
+            if (parseInt(req.headers["content-length"]) > 690) {
+                res.status(413).send({
+                    status: 413,
+                    message: "Tamaño de la solicitud alcanzado"
+                });
+                return true;
+            }
+            return false;
+        },
+        message: (req:any, res:Response) => {
+            res.status(429).send({
+                status: 429,
+                message: "Limite alcanzado"
+            });
+        }
+    });
+}
+
+export let post = () => {
+    return rateLimit({
+        windowMs:  30 * 60 * 1000,
+        max: 5,
+        standardHeaders: true,
+        legacyHeaders: false,
+        skip: (req: any, res: Response) => {
+            if (parseInt(req.headers["content-length"]) > 690) {
+                res.status(413).send({
+                    status: 413,
+                    message: "Tamaño de la solicitud alcanzado"
+                });
+                return true;
+            }
+            return false;
+        },
+        message: (req:any, res:Response) => {
+            res.status(429).send({
+                status: 429,
+                message: "Limite alcanzado"
+            });
+        }
+    });
+}
+
+export let deleteLimiter = () => {
+    return rateLimit({
+        windowMs:   3600 * 1000,
+        max: 5,
+        standardHeaders: true,
+        legacyHeaders: false,
+        skip: (req: any, res: Response) => {
+            if (parseInt(req.headers["content-length"]) > 690) {
+                res.status(413).send({
+                    status: 413,
+                    message: "Tamaño de la solicitud alcanzado"
+                });
+                return true;
+            }
+            return false;
+        },
+        message: (req:any, res:Response) => {
+            res.status(429).send({
+                status: 429,
+                message: "Limite alcanzado"
+            });
+        }
+    });
+}
